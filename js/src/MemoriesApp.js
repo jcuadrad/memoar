3bcffb0c931dbac05da579210196f747b26dc861
Juan Cuadra <jcb8793@gmail.com>
2019-02-04 22:20:56 -0600

    Add Router

--- js/src/MemoriesApp.js
+++ js/src/MemoriesApp.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Image, Text, TouchableHighlight } from 'react-native';
+import { View } from 'react-native';
+import { NativeRouter, Route, Switch } from "react-router-native";
 
-import splash from '../res/SplashPhoto.png';
+import Splash from './Splash';
 
 class MemoriesApp extends Component {
     constructor(props) {
@@ -11,113 +12,15 @@ class MemoriesApp extends Component {
     render() { 
         const { enterAR, ar } = this.props;
         return ( 
-            // <View style={localStyles.container} >
-            //     <View style={localStyles.photo} >
-        
-            //         <Text style={localStyles.titleText}>
-            //             Choose your desired experience:
-            //         </Text>
-            
-            //         <TouchableHighlight style={localStyles.buttons}
-            //             onPress={enterAR(ar)}
-            //             underlayColor={'#68a0ff'} >
-            
-            //             <Text style={localStyles.buttonText}>AR</Text>
-            //         </TouchableHighlight>
-            //     </View>
-            // </View>
-            <View style={localStyles.viroContainer} >
-                <Image style={localStyles.photo} source={splash}></Image>
-                <View style={localStyles.titleContainer}>
-                    <Text style={localStyles.title}>
-                        MEMO
-                        <Text style={localStyles.titleEmphasis}>AR</Text>
-                    </Text>
-                    <Text style={localStyles.paragraph}>An app that brings your best memories back to life.</Text>
-                    <TouchableHighlight style={localStyles.buttons} onPress={enterAR(ar)} underlayColor={'#68a0ff'}>
-                        <Text style={localStyles.buttonText}>ENTER</Text>
-                    </TouchableHighlight>
+            <NativeRouter>
+                <View style={{flex: 1}}>
+                    <Switch>
+                        <Route exact path="/" render={(props) => <Splash {...props} enterAR={enterAR} ar={ar} />} />
+                    </Switch>
                 </View>
-            </View>
+            </NativeRouter>
          );
     }
 }
-
-
-var localStyles = StyleSheet.create({
-    viroContainer :{
-      flex : 1,
-      backgroundColor: "white",
-    },
-    container: {
-      flex : 1,
-      flexDirection: 'row',
-      alignItems:'center',
-      backgroundColor: "black",
-    },
-    photo: {
-      flex : 4,
-      backgroundColor: "steelblue",
-    },
-    titleContainer: {
-      flex: 2,
-      padding: 30,
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      justifyContent: 'center',
-      backgroundColor: '#3B3B3B'
-    },
-    title: {
-        color: 'white',
-        fontSize: 50,
-        fontWeight: 'bold',
-        fontStyle: 'italic',
-        marginBottom: 20,
-    },
-    paragraph: {
-        color: '#818181',
-        fontSize: 16,
-        fontWeight: '200'
-    },
-    titleEmphasis: {
-        color: '#DF4E40'
-    },
-    titleText: {
-      paddingTop: 30,
-      paddingBottom: 20,
-      color:'#fff',
-      textAlign:'center',
-      fontSize : 25
-    },
-    buttonText: {
-      color:'#fff',
-      textAlign:'center',
-      fontSize : 17
-    },
-    buttons : {
-      height: 50,
-      width: 150,
-      paddingTop: 0,
-      paddingBottom: 0,
-      flexDirection: 'row',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 25,
-      marginBottom: 20,
-      backgroundColor:'#DF4E40'
-    },
-    exitButton : {
-      height: 50,
-      width: 100,
-      paddingTop:10,
-      paddingBottom:10,
-      marginTop: 10,
-      marginBottom: 10,
-      backgroundColor:'#68a0cf',
-      borderRadius: 10,
-      borderWidth: 1,
-      borderColor: '#fff',
-    }
-  });
  
 export default MemoriesApp;
---
+++ js/src/Splash.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Image, Text, TouchableHighlight } from 'react-native';
+
+import splash from '../res/SplashPhoto.png';
+
+class Splash extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        const { enterAR, ar } = this.props;
+        return ( 
+            <View style={localStyles.viroContainer} >
+                <Image style={localStyles.photo} source={splash}></Image>
+                <View style={localStyles.titleContainer}>
+                    <Text style={localStyles.title}>
+                        MEMO
+                        <Text style={localStyles.titleEmphasis}>AR</Text>
+                    </Text>
+                    <Text style={localStyles.paragraph}>An app that brings your best memories back to life.</Text>
+                    <TouchableHighlight style={localStyles.buttons} onPress={enterAR(ar)} underlayColor={'#68a0ff'}>
+                        <Text style={localStyles.buttonText}>ENTER</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+         );
+    }
+}
+
+var localStyles = StyleSheet.create({
+    viroContainer :{
+      flex : 1,
+      backgroundColor: "white",
+    },
+    container: {
+      flex : 1,
+      flexDirection: 'row',
+      alignItems:'center',
+      backgroundColor: "black",
+    },
+    photo: {
+      flex : 4,
+      backgroundColor: "steelblue",
+    },
+    titleContainer: {
+      flex: 2,
+      padding: 30,
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'center',
+      backgroundColor: '#3B3B3B'
+    },
+    title: {
+        color: 'white',
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+        marginBottom: 20,
+    },
+    paragraph: {
+        color: '#818181',
+        fontSize: 16,
+        fontWeight: '200'
+    },
+    titleEmphasis: {
+        color: '#DF4E40'
+    },
+    titleText: {
+      paddingTop: 30,
+      paddingBottom: 20,
+      color:'#fff',
+      textAlign:'center',
+      fontSize : 25
+    },
+    buttonText: {
+      color:'#fff',
+      textAlign:'center',
+      fontSize : 17
+    },
+    buttons : {
+      height: 50,
+      width: 150,
+      paddingTop: 0,
+      paddingBottom: 0,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 25,
+      marginBottom: 20,
+      backgroundColor:'#DF4E40'
+    },
+    exitButton : {
+      height: 50,
+      width: 100,
+      paddingTop:10,
+      paddingBottom:10,
+      marginTop: 10,
+      marginBottom: 10,
+      backgroundColor:'#68a0cf',
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: '#fff',
+    }
+  });
+ 
+export default Splash;
